f6191cd43b3a7fe7f57b81eda187aaf9d00a2bf5
Havystar <31964929+Havystar@users.noreply.github.com>
2021-07-06 23:58:55 +0200

    axios getting data from API, form for searching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=25&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="currency-app">
+      <header className="currency-header">
         <h1>React currency app</h1>
       </header>
+      <div className="currency-search">
+        <p>Search for a currency</p>
+        <form>
+          <input
+            type="text"
+            className="currency-input"
+            placeholder="Search"
+          ></input>
+        </form>
+      </div>
     </div>
   );
 }
